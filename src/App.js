9509c8fa9b93c3f9b1f2e5d3ead25df6897b424f
ahnnaeng <dksduddms0@gmail.com>
2025-04-15 16:32:06 +0900

    Fix: set basename for GitHub Pages routing3

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import Detail from "./routes/Detail";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/movie/:id" element={<Detail />}></Route>
-        <Route path="/" element={<Home />}></Route>
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/movie/:id" element={<Detail />} />
+      <Route path="/" element={<Home />} />
+    </Routes>
   );
 }
 
