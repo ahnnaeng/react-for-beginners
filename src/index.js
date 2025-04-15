0de015c28b596a9653e3c8dcb5041abf10a18724
ahnnaeng <dksduddms0@gmail.com>
2025-04-15 16:41:58 +0900

    fix: dynamic basename for local and GitHub Pages

--- src/index.js
+++ src/index.js
@@ -3,9 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
+const basename =
+  process.env.NODE_ENV === "production" ? "/react-for-beginners" : "/";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
+console.log("NODE_ENV - ", process.env.NODE_ENV);
 root.render(
-  <BrowserRouter basename="/">
+  <BrowserRouter basename={basename}>
     <App />
   </BrowserRouter>
 );
